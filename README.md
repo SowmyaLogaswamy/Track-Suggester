80af7cad1d35fe34e88ea18624fbbd53c79e1099
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-06-09 14:44:05 -0700

    Add README file

---
+++ README.md
@@ -0,0 +1,40 @@
+# Portfolio
+A portfolio of Sowmya
+# _Track Suggester-Epicodus_
+
+#### _This project is all about my the track suggester. It helps the students those who are confused to choose the right track, 06.09.2017_
+
+#### By _**Sowmya Devi Logaswamy**_
+
+## Description
+
+_This project is a track suggester. It helps the people to choose the best track that suits them. It guides them to choose a track in which they can excel._
+
+## Setup/Installation Requirements
+
+* _Initialize git_
+* _Create files using HTML_
+* _Create a CSS file for styling_
+* _Use bootstrap framework for responsiveness_
+* _Use jquery effects to the web page
+* _Then commit the code and push it to the hub_
+
+_We should install atom editor, terminal, create github account, download bootstrap and css _Leave nothing to chance!  It would be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?_
+
+## Known Bugs
+
+_Sometimes pictures may not display_
+
+## Support and contact details
+
+_Let me know what to do if you run into any issues or have questions, ideas or concerns.  contact at sowmyalogaswamy@gmail.com.You can view the link at https://github.com/SowmyaLogaswamy/Track-Suggester _
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, Jquery_
+
+### License
+
+*Except as noted, this content is licensed under Sowmya Devi Logaswamy. For details and restrictions, see the Content License.*
+
+Copyright (c) 2016 **_Sowmya Logaswamy_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#tracksuggesterform").submit(function(event) {
-    if($("#interest").val()==="choose")
+    if($("#interest").val()==="choose" || $("#skills").val()==="choose" || $("#job").val()==="choose")
     {
-      alert("SORRY!! YOU HAVE MISSED OUT A TO FILL A TEXT BOX!");
+      alert("SORRY!! YOU HAVE MISSED OUT TO FILL IN A TEXT BOX!");
     }
     var name=$("#name").val();
     var interest=$("#interest").val();
@@ -16,15 +16,15 @@ $(document).ready(function() {
         $("#abouttrack").hide();
         $("#trackname").text("CSS/Design");
         $("#abouttrack").text(name + "," + " HTML (the Hypertext Markup Language) and CSS (Cascading Style Sheets) are two of the core technologies for building Web pages. HTML provides the structure of the page, CSS the (visual and aural) layout, for a variety of devices. Along with graphics and scripting, HTML and CSS are the basis of building Web pages and Web Applications");
-        $("#trackname").delay(250).fadeIn(500);
-        $("#abouttrack").delay(250).fadeIn(500);
+        $("#trackname").delay(100).slideToggle(500);
+        $("#abouttrack").delay(100).slideToggle(500);
       }
     else if(interest==="applications" && skills==="newlanguages" && job==="developer")
     {
       $("#trackname").text("Ruby on Rails");
       $("#abouttrack").text(name + "," + " Ruby is a favorite language of developers building interactive web applications. If an app involves users creating accounts, entering information, and interacting with dynamic content, there's a good chance it is built with Ruby. Ruby became popular because the Rails framework, which is written with Ruby, simplified many of the common tasks associated with building web applications. It's most popular with startups and smaller companies who are looking to build their product quickly.Though each language has its niche, there is plenty of crossover. For example, Rails' popularity inspired copycats in just about every language, and so you'll see interactive web applications written in C#, Java, and PHP, with Rails-like frameworks including .NET MVC, Spring, and Laravel. Even at one company, you might find them using PHP for their marketing site, Ruby for their web application, and Java for their back-end processing.");
-      $("#trackname").delay(250).fadeIn(500);
-      $("#abouttrack").delay(250).fadeIn(500);
+      $("#trackname").delay(250).fadeIn(250);
+      $("#abouttrack").delay(250).fadeIn(250);
     }
     else if(interest==="animation" && skills==="games" && job==="gamer")
     {
@@ -38,8 +38,8 @@ $(document).ready(function() {
   {
     $("#trackname").text("Ruby on Rails");
     $("#abouttrack").text(name + "," + " Ruby is a favorite language of developers building interactive web applications. If an app involves users creating accounts, entering information, and interacting with dynamic content, there's a good chance it is built with Ruby. Ruby became popular because the Rails framework, which is written with Ruby, simplified many of the common tasks associated with building web applications. It's most popular with startups and smaller companies who are looking to build their product quickly.                                     Though each language has its niche, there is plenty of crossover. For example, Rails' popularity inspired copycats in just about every language, and so you'll see interactive web applications written in C#, Java, and PHP, with Rails-like frameworks including .NET MVC, Spring, and Laravel. Even at one company, you might find them using PHP for their marketing site, Ruby for their web application, and Java for their back-end processing.");
-    $("#trackname").delay(250).fadeIn(500);
-    $("#abouttrack").delay(250).fadeIn(500);
+    $("#trackname").delay(100).fadeOut(250);
+    $("#abouttrack").delay(100).fadeOut(250);
   }
       event.preventDefault();
   });
